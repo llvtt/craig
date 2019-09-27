fe3384df101ca4885a3faef6c06d1c1cdf70c204
Liz Bennett <liz@bennettworld.org>
2019-09-27 08:59:03 -0700

    Searches can now be initiated from the http server

---
+++ searcher.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+const CONFIG_FILE_NAME = "config.json"
+
+func search() {
+	config := parseConfig(CONFIG_FILE_NAME)
+	craigslistClient := NewCraigslistClient("sfbay")
+	slackClient := NewSlackClient()
+
+	options := &SearchOptions{HasPicture: true, SubRegion: config.Region}
+	for _, search := range config.Searches {
+		options.Neighborhoods = search.Neighborhoods
+		categoryClient := craigslistClient.Category(search.Category).Options(options)
+		for _, term := range search.Terms {
+			var newResults Listing
+			for _, result := range categoryClient.Search(term) {
+				if craigslistClient.Insert(result) {
+					newResults = append(newResults, result)
+				}
+			}
+			if len(newResults) > 0 {
+				announcement := fmt.Sprintf("Found %d new *free* items on my list!", len(newResults))
+				if len(term) > 0 {
+					announcement = fmt.Sprintf("Found %d new items matching *%s* on my list!", len(newResults), term)
+				}
+				slackClient.SendString(announcement)
+				for _, result := range newResults {
+					slackClient.SendItem(result)
+				}
+			}
+		}
+	}
+	craigslistClient.flushDB()
+}
--- server.go
+++ server.go
@@ -6,22 +6,36 @@ import (
 	"net/http"
 )
 
-func listSearches(w http.ResponseWriter, r *http.Request) {
+func listSearchesHandler(w http.ResponseWriter, r *http.Request) {
 	config := parseConfig(CONFIG_FILE_NAME)
 	searches, _ := json.Marshal(config.Searches)
 	w.Write(searches)
 }
 
-func health(w http.ResponseWriter, r *http.Request) {
+func runSearchHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		methodNotFoundResponse(w)
+	}
+
+	search()
+}
+
+func healthHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("{\"status\":\"healthy\"}"))
 }
 
+func methodNotFoundResponse(w http.ResponseWriter) {
+	http.Error(w, "{\"status\":\"error\", \"message\":\"No such http route\"}", http.StatusNotFound)
+}
+
 func initEndpoints() {
-	http.HandleFunc("/searches", listSearches)
-	http.HandleFunc("/health", health)
+	http.HandleFunc("/searches", listSearchesHandler)
+	http.HandleFunc("/search", runSearchHandler)
+	http.HandleFunc("/health", healthHandler)
 }
 
 func startServer() {
+	log.Print("Starting server...")
 	initEndpoints()
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
