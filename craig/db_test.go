26dc0315b06586a2e5804aa9618baf098eb5aff1
Liz Bennett <lbennett@confluent.io>
2020-02-16 11:21:53 -0800

    Started the test for db logic

--- craig/db_test.go
+++ craig/db_test.go
@@ -3,11 +3,20 @@ package craig
 import (
 	"context"
 	"github.com/go-kit/kit/log"
+	"github.com/llvtt/craig/types"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/suite"
 	"os"
 	"testing"
+	"time"
 )
 
+const (
+	TEST_DB_FILE = "./test.db.json"
+	TS_FORMAT    = time.RFC822
+	INDEX_DATE   = "16 Feb 20 01:00 PST"
+	PUBLISH_DATE = "16 Feb 20 00:00 PST"
+)
 
 type DBTestSuite struct {
 	suite.Suite
@@ -17,6 +26,13 @@ type DBTestSuite struct {
 
 func (suite *DBTestSuite) SetupTest()  {
 	suite.logger = log.With(log.NewJSONLogger(os.Stdout), "app", "flow")
+	// clean up after previous runs if necessary
+	os.Remove(TEST_DB_FILE)
+}
+
+
+func (suite *DBTestSuite) TearDownTest()  {
+	os.Remove(TEST_DB_FILE)
 }
 
 
@@ -26,9 +42,84 @@ func TestDBTestSuite(t *testing.T) {
 	})
 }
 
+func (suite *DBTestSuite) TestNew_DB_Client_Invalid_Config() {
+	t := suite.T()
+	conf := &types.CraigConfig{}
+	c, err := NewDBClient(conf, suite.logger)
+	assert.NotNil(t, err)
+	assert.Contains(t, err.Error(), "no db type specified. must specify db_type in config file")
+	assert.Nil(t, c)
+
+	conf = &types.CraigConfig{DBType: "derp"}
+	c, err = NewDBClient(conf, suite.logger)
+	assert.NotNil(t, err)
+	assert.Contains(t, err.Error(), "invalid db type: derp")
+	assert.Nil(t, c)
+
+	conf = &types.CraigConfig{DBType: "json"}
+	c, err = NewDBClient(conf, suite.logger)
+	assert.NotNil(t, err)
+	assert.Contains(t, err.Error(), "could not open db file")
+	assert.Nil(t, c)
+}
+
 func (suite *DBTestSuite) TestNew_DB_Client() {
-	//c, err := NewDBClient(nil, suite.logger)
+	t := suite.T()
+	conf := &types.CraigConfig{DBType: "json", DBFile: TEST_DB_FILE}
+	c, err := NewDBClient(conf, suite.logger)
+	assert.Nil(t, err)
+	assert.NotNil(t, c)
+}
 
 
+func (suite *DBTestSuite) TestDB_Inserts() {
+	t := suite.T()
+	conf := &types.CraigConfig{DBType: "json", DBFile: TEST_DB_FILE}
+	c, err := NewDBClient(conf, suite.logger)
+	assert.Nil(t, err)
+
+	indexDate, _ := time.Parse(TS_FORMAT, INDEX_DATE)
+	publishDate, _ := time.Parse(TS_FORMAT, PUBLISH_DATE)
+	// test inserting an item
+	item := &types.CraigslistItem{
+		Url: "fake url",
+		Title: "fake title",
+		Description: "fake description",
+		ThumbnailUrl: "thumbnail url",
+		IndexDate: indexDate,
+		PublishDate: publishDate,
+		Price: 10.0,
+	}
+	inserted, err := c.InsertSearchedItem(item)
+	assert.NoError(t, err)
+	assert.True(t, inserted)
+
+	// try inserting another item with the same title, it should not insert anything
+	item2 := &types.CraigslistItem{
+		Url: "fake url2",
+		Title: "fake title",
+		Description: "fake description. different listing but same title",
+		ThumbnailUrl: "thumbnail url",
+		IndexDate: indexDate,
+		PublishDate: publishDate,
+		Price: 10.0,
+	}
+	inserted, err = c.InsertSearchedItem(item2)
+	assert.NoError(t, err)
+	assert.False(t, inserted)
+
+	// try inserting another item with the same url, it should not insert anything
+	item3 := &types.CraigslistItem{
+		Url: "fake url",
+		Title: "fake title, different listing same url",
+		Description: "fake description",
+		ThumbnailUrl: "thumbnail url",
+		IndexDate: indexDate,
+		PublishDate: publishDate,
+		Price: 10.0,
+	}
+	inserted, err = c.InsertSearchedItem(item3)
+	assert.NoError(t, err)
+	assert.False(t, inserted)
 }
 
--- go.mod
+++ go.mod
@@ -11,5 +11,6 @@ require (
 	github.com/mattn/go-sqlite3 v2.0.3+incompatible
 	github.com/mmcdole/gofeed v1.0.0-beta2
 	github.com/mmcdole/goxpp v0.0.0-20181012175147-0068e33feabf // indirect
+	github.com/stretchr/testify v1.4.0
 	golang.org/x/text v0.3.2 // indirect
 )
